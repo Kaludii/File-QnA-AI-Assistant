74e60bf946fcfc1bd721a93541369e565e2d68d9
Bilal Kaludi <63890666+Kaludii@users.noreply.github.com>
2023-03-14 21:11:45 -0500

    added ability for response to be streamed

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ import io
 from PyPDF2 import PdfReader
 from PIL import Image
 
-# Add a title and description
 st.title("File Q&A AI Assistant")
-st.write("This app allows you to upload a CSV or PDF file, or enter text and ask questions related to the content. The app uses OpenAI's ChatGPT model to assist you in answering your questions about the uploaded content.")
+st.write("This app allows you to upload a CSV or PDF file, or enter text and ask questions related to the content. The app uses OpenAI's ChatGPT model to assist you in answering your questions about the uploaded content, which are streamed back in real time similar to the ChatGPT interface.")
 
 messages = [
     {"role": "system", "content": "You are a professional Question and Answer AI Assistant helping with information in regards to a csv, pdf, and text input file."},
@@ -19,57 +18,50 @@ def chatbot(api_key, query_text, file_data):
         messages.append({"role": "user", "content": query_text})
     if file_data:
         messages.append({"role": "user", "content": f"{file_type} File Type: {file_data}"})
+    
     chat = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=messages
+        model="gpt-3.5-turbo", messages=messages, stream=True
     )
-    reply = chat.choices[0].message.content
-    messages.append({"role": "assistant", "content": reply})
-    if "InvalidRequestError" in reply:
-        raise Exception(reply)
-    return reply
+    
+    response_text = st.empty()
+    response_line = ""
+    
+    for chunk in chat:
+        chunk_message = chunk['choices'][0]['delta']
+        if chunk_message.get('content'):
+            response_line += chunk_message['content']
+            response_text.write("Response: " + response_line)
+    
+    messages.append({"role": "assistant", "content": response_line})
 
 api_key = st.text_input("OpenAI API Key", type="password", key=2)
 query_text = st.text_area("Question", key="input", height=150)
 file_type = st.selectbox("Select File Type", options=["CSV", "PDF", "Text"])
 
-# Initialize file_data variable with a default value of None
 file_data = None
 
 if file_type == "CSV":
     file = st.file_uploader("Upload CSV file", type="csv")
     if file:
-        # read the csv file into a pandas dataframe
         df = pd.read_csv(file)
         st.write("Uploaded CSV file:")
-        # display the dataframe as a table
         st.write(df)       
-        # update file_data with csv data
         file_data = df.to_csv(index=False)
 elif file_type == "PDF":
     file = st.file_uploader("Upload PDF file", type="pdf")
     if file:
-        # Initialize a PyPDF2 PdfReader object
         pdf_reader = PdfReader(file)
-
-        # Extract the text from the PDF file
         file_data = ""
         for page in pdf_reader.pages:
             file_data += page.extract_text()
-
-        # Display the uploaded PDF file
         st.write("Uploaded PDF file:")
         st.write(file_data)
-
 else:
     file_data = st.text_area("Enter text here")
 
-output_text = st.empty()
-
 if st.button("Send"):
     try:
-        response = chatbot(api_key, query_text, file_data)
-        st.write("Response:")
-        st.write(response)
+        chatbot(api_key, query_text, file_data)
     except Exception as e:
         st.error(str(e))
 
@@ -77,3 +69,11 @@ st.markdown("")
 st.markdown("---")
 st.markdown("")
 st.markdown("<p style='text-align: center'><a href='https://github.com/Kaludii'>Github</a> | <a href='https://huggingface.co/Kaludi'>HuggingFace</a></p>", unsafe_allow_html=True)
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
